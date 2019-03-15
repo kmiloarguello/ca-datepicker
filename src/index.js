b3154f2a75890640a7a34cba1801dd84ceb4441b
Camilo Arguello Cencosud <camiloandres.arguellorincon@cencosud.com.co>
2019-03-14 21:39:11 -0500

    creando eventos

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 class CAdatepicker{
     constructor(){
         this.init = this.init.bind(this);
+
+        this.days = ["Do","Lu","Ma","Mi","Ju","Vi","Sa"];
+        this.months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+        this.events = ["click"];
     }
     init(){
         console.log("Hola Camilo");
     }
+    renderCalendar(){
+        const container = document.createElement("div");
+        
+    }
+    createControls(){}
+
 }
 
 const ca = new CAdatepicker();
